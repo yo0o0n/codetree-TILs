a7105021e03a810c4ab551c283324fc844d58652
yo0o0n <137882696+yo0o0n@users.noreply.github.com>
2024-10-08 15:27:55 +0900

    Title: 예술성, Time: 8ms, Memory: 3MB, Status: Passed - CodeTree

--- 241008/예술성/artistry.cpp
+++ 241008/예술성/artistry.cpp
@@ -106,6 +106,8 @@ void rotate() {
 }
 
 int main(void) {
+    ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
     cin >> n;
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++)
